aca5016f14af3e1b5689fd85f6a2a190d4d398b8
Victor Bozhuk <bozhook@gmail.com>
2020-07-25 15:15:30 +0300

    fixed some methods.

--- WorldOfWords.Tests/WordServiceTest.cs
+++ WorldOfWords.Tests/WordServiceTest.cs
@@ -11,11 +11,6 @@ namespace WorldOfWords.Tests
     public class WordServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
-        {
-        }
-
-        [TestMethod]
         public void DatabaseReturnsAllCountOfWords()
         {
             var mock = new Mock<IWordService>();
@@ -23,38 +18,6 @@ namespace WorldOfWords.Tests
             var service = mock.Object;
 
             Assert.AreEqual(GetTestWords().Count, service.GetAllWords().Count);
-        }
-
-        [TestMethod]
-        public void CountOfWordsWillBeIncreasedWhenAddedRangeOfWords()
-        {
-            var mock = new Mock<IWordService>();
-            mock.Setup(repo => repo.GetAllWords()).Returns(GetTestWords());
-            var db = mock.Object;
-
-            int oldCount = db.GetAllWords().Count;
-            db.AddRange(new Word[2]
-                { new Word { Id = new Guid("d7b727f8-373b-4145-8cd9-f0ec0d09d098"), Name = "Samsung5", Translate = "Самсунг5" },
-                new Word { Id = new Guid("3fb6a621-256f-4c92-8f21-8e725590f749"), Name = "Samsung6", Translate = "Самсунг6" },
-                });
-
-            Assert.AreEqual(oldCount + 2, db.GetAllWords().Count);
-        }
-
-        [TestMethod]
-        public void CountOfWordsWillBeIncreasedWhenAddedRangeOfWords2()
-        {
-            var mock = new Mock<IWordService>();
-            mock.Setup(repo => repo.GetAllWords()).Returns(GetTestWords());
-            var db = mock.Object;
-            
-            int oldCount = db.GetAllWords().Count;
-            db.AddRange(new Word[2]
-                { new Word { Id = new Guid("d7b727f8-373b-4145-8cd9-f0ec0d09d098"), Name = "Samsung5", Translate = "Самсунг5" },
-                new Word { Id = new Guid("3fb6a621-256f-4c92-8f21-8e725590f749"), Name = "Samsung6", Translate = "Самсунг6" },
-                });
-
-            Assert.AreEqual(oldCount + 2, db.GetAllWords().Count);
         }
 
         private List<Word> GetTestWords()
